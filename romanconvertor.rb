2ed445df7c49d01392305168c956401cfe5b042b
cody <cminer145@gmail.com>
2020-09-04 02:44:08 -0400

    Added naive toRoman() that doesn't know about subtraction, e.g. 4 -> IIII

--- romanconvertor.rb
+++ romanconvertor.rb
@@ -9,15 +9,32 @@ def fromRoman(romanNumber)
         "M" => 1000
     }
 
+    # pairedLetterValues = {
+    # 	"IV" => 4,
+    # 	"IX" => 9,
+    # 	"XL" => 40,
+    # 	"XC" => 90,
+    # 	"CD" => 400,
+    # 	"CM" => 900
+    # }
+
     total = 0
 
-    i = romanNumber.length
-    while i > 0 do
-    	if not "IVXLCDM".include? romanNumber[i-1] 
+    # i = romanNumber.length - 2
+    # while i >= 0 do
+    # 	if i+1 < romanNumber.length and pairedLetterValues[romanNumber[i..i+1]] != nil
+    # 		total += 
+    # 	end
+    # 	i -= 1
+    # end
+
+    j = romanNumber.length - 1
+    while j >= 0 do
+    	if not "IVXLCDM".include? romanNumber[j] 
     		raise TypeError
     	end
-    	total += singleLetterValues[romanNumber[i-1]]
-    	i -= 1
+    	total += singleLetterValues[romanNumber[j]]
+    	j -= 1
     end
 
     return total
@@ -25,7 +42,57 @@ def fromRoman(romanNumber)
 end
 
 def toRoman(arabicNumber)
+	valueLetters = {
+        1 => "I",
+        5 => "V",
+        10 => "X",
+        50 => "L",
+        100 => "C",
+        500 => "D",
+        1000 => "M"
+    }
+
+    singleLetters = "IVXLCDM"
+
+    singleLetterValues = {
+        "I" => 1,
+        "V" => 5,
+        "X" => 10,
+        "L" => 50,
+        "C" => 100,
+        "D" => 500,
+        "M" => 1000
+    }
+
     if arabicNumber < 1 or arabicNumber > 3999
     	raise RangeError
     end
+
+    output = ""
+    i = singleLetters.length - 1
+    while i >= 0 do
+    	letter = singleLetters[i]
+    	value = singleLetterValues[letter]
+    	output += letter * (arabicNumber / value)
+    	arabicNumber = arabicNumber % value
+    	i -= 1
+    end
+    return output
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
