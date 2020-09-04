3a1c5c73db10c30ad9ce82b58df410313725af7f
cody <cminer145@gmail.com>
2020-09-04 01:48:35 -0400

    Added naive fromRoman conversion that ignores subtraction, e.g. IV becomes 6, not 4.

--- romanconvertor.rb
+++ romanconvertor.rb
@@ -1,9 +1,31 @@
 def fromRoman(romanNumber)
-    # Replace the following line with the actual code!
-    return ""
+	singleLetterValues = {
+        "I" => 1,
+        "V" => 5,
+        "X" => 10,
+        "L" => 50,
+        "C" => 100,
+        "D" => 500,
+        "M" => 1000
+    }
+
+    total = 0
+
+    i = romanNumber.length
+    while i > 0 do
+    	if not "IVXLCDM".include? romanNumber[i-1] 
+    		raise TypeError
+    	end
+    	total += singleLetterValues[romanNumber[i-1]]
+    	i -= 1
+    end
+
+    return total
+    
 end
 
 def toRoman(arabicNumber)
-    # Replace the following line with the actual code!
-    return ""
+    if arabicNumber < 1 or arabicNumber > 3999
+    	raise RangeError
+    end
 end
